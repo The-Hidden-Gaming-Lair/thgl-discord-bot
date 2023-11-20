bf0d02d19f78cf723e1d7c60fddd8bfbaf55a05f
lmachens <leon@machens.koeln>
2023-11-20 15:26:14 +0100

    POC Mutation Cycle API

---
+++ index.ts
@@ -0,0 +1,40 @@
+import { Client, Events, GatewayIntentBits, TextChannel } from "discord.js";
+
+const MUTATION_CYCLE_CHANNEL_ID = "1062724443580936192";
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+async function getLatestMutationCycle() {
+  const channel = client.channels.cache.get(
+    MUTATION_CYCLE_CHANNEL_ID
+  ) as TextChannel;
+  if (!channel) {
+    throw new Error(`Channel ${MUTATION_CYCLE_CHANNEL_ID} not found`);
+  }
+  const messages = await channel.messages.fetch({ limit: 3 });
+  return messages.map((message) => {
+    const expedition = message.content.split("**")[1];
+    const mutations = message.content.split("`")[1].split(", ");
+    return {
+      expedition,
+      mutations,
+      imageSrc: message.attachments.at(0)?.url,
+    };
+  });
+}
+Bun.serve({
+  async fetch(req) {
+    const url = new URL(req.url);
+    if (url.pathname === "/mutation-cycle") {
+      const latestMutationCycle = await getLatestMutationCycle();
+      return Response.json(latestMutationCycle);
+    }
+    return new Response("404!");
+  },
+});
