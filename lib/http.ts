cf319c3bd51eea735c1f76247538c65a12aa6f42
lmachens <leon@machens.koeln>
2023-11-21 11:05:54 +0100

    Cache response

--- lib/http.ts
+++ lib/http.ts
@@ -1,21 +1,22 @@
 import { BodyInit, ResponseInit } from "undici-types";
 
-const CORS_HEADERS = {
+const RESPONSE_HEADERS = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "OPTIONS, GET",
   "Access-Control-Allow-Headers": "Content-Type",
+  "Cache-Control": "public, max-age=60, s-maxage=60",
 };
 
 export class ClientResponse extends Response {
   constructor(body?: BodyInit, init?: ResponseInit) {
     super(body, init);
-    Object.entries(CORS_HEADERS).forEach(([key, value]) => {
+    Object.entries(RESPONSE_HEADERS).forEach(([key, value]) => {
       this.headers.set(key, value);
     });
   }
 
   static json(data: any, init: ResponseInit = {}): Response {
-    Object.assign(init, { headers: CORS_HEADERS });
+    Object.assign(init, { headers: RESPONSE_HEADERS });
     return super.json(data, init);
   }
 }
