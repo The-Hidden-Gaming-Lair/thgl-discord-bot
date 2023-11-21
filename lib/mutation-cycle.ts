fafc478a3d0f8b7c67e15390931236af4a7d6d39
lmachens <leon@machens.koeln>
2023-11-21 10:20:14 +0100

    Refactor functional calls

--- lib/mutation-cycle.ts
+++ lib/mutation-cycle.ts
@@ -1,30 +1,30 @@
+import { Message } from "discord.js";
 import { getChannelMessages } from "./discord";
 
 const MUTATION_CYCLE_CHANNEL_ID = "1062724443580936192";
 
 export async function getLatestMutationCycle() {
   const messages = await getChannelMessages(MUTATION_CYCLE_CHANNEL_ID, 3);
-  return messages
-    .map((message) => {
-      try {
-        const content = message.content.split("\n").map((line) => {
-          const expedition = line.split("**")[1];
-          const mutations = line.split("`")[1].split(", ");
-          return {
-            expedition,
-            mutations,
-          };
-        });
-        return {
-          content,
-          imageSrc: message.attachments.at(0)?.url,
-          timestamp: message.createdTimestamp,
-        };
-      } catch (error) {
-        // Ignore invalid messages
-        return null;
-      }
-    })
-    .filter(Boolean)
-    .at(0);
+  return messages.map(toMutationCycle).filter(Boolean).at(0);
+}
+
+function toMutationCycle(message: Message) {
+  try {
+    const content = message.content.split("\n").map((line) => {
+      const expedition = line.split("**")[1];
+      const mutations = line.split("`")[1].split(", ");
+      return {
+        expedition,
+        mutations,
+      };
+    });
+    return {
+      content,
+      imageSrc: message.attachments.at(0)?.url,
+      timestamp: message.createdTimestamp,
+    };
+  } catch (error) {
+    // Ignore invalid messages
+    return null;
+  }
 }
