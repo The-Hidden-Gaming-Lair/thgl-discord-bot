1c7883b00292338b32a74efc7a16fd3b4b0f980f
lmachens <leon@machens.koeln>
2025-09-25 13:29:16 +0200

    Remove mutation cycle

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import { getClient, initDiscord } from "./lib/discord";
 import { ClientResponse } from "./lib/http";
 import { handleInfo } from "./routes/info/route";
-import { handleMutationCycle } from "./routes/mutation-cycle/route";
 import { handleUpdates } from "./routes/updates/route";
 
 await initDiscord();
@@ -11,9 +10,6 @@ console.log(`Ready! Logged in as ${client.user.tag}`);
 Bun.serve({
   async fetch(req) {
     const url = new URL(req.url);
-    if (url.pathname.startsWith("/api/mutation-cycle")) {
-      return handleMutationCycle(req, url);
-    }
     if (url.pathname.startsWith("/api/updates")) {
       return handleUpdates(req, url);
     }
